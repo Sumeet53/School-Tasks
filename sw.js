b289a26c50ac212e4d81b97009898b5d44de8923
Sumeet53 <immplg204@gmail.com>
2026-09-22 19:46:08 +0530

    Update cache name to schooltasks-cache-v13

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    (with the last-loaded data) even with a poor/no connection.
    ========================================================= */
 
-const CACHE_NAME = "schooltasks-cache-v12";
+const CACHE_NAME = "schooltasks-cache-v13";
 
 const FILES_TO_CACHE = [
   "./index.html",
